2e0a52461f0b995cc51e25617c6e032ed232013f
Ilde Stramandinoli <109019707+ildestram@users.noreply.github.com>
2022-09-29 16:01:38 +0200

    esercizio corretto

---
+++ js/scriptex.js
@@ -0,0 +1,33 @@
+let items = document.getElementById('items');
+
+function bold() {
+    let itemsSelected = document.getElementsByClassName('item');
+
+    for(let item of itemsSelected) {
+        item.classList.add('bold');
+    }
+}
+
+function italics() {
+    let itemsSelected = document.getElementsByClassName('item');
+
+    for(let item of itemsSelected) {
+        item.classList.add('italics');
+    }
+}
+
+function getInputText(eventObject) {
+    if(eventObject.key !== 'Enter') {
+        return;
+    }
+
+    let input = document.getElementById('input-text');
+    if (input.value !== '') {
+        items.innerHTML += `<div class="item" onclick="selectItem(event)">${input.value}</div>`;
+        input.value = '';
+    } else {
+        alert("E' impossibile che non hai più esami da sostenere")
+        input.style.borderColor = 'black';
+    }
+
+}
